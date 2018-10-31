0bddfba66af6c1d127a6e0e43a1c6f1e0dadc90d
Rodrigo <rodrigogribeiro@gmail.com>
2018-10-31 09:32:38 -0300

    cleaning

--- chapter2/guessing_game/target/debug/deps/guessing_game-c10f4be7107468c3.d
+++
@@ -1,5 +0,0 @@
-/Users/rodrigo/Dropbox/projects/rust/rust-playground/chapter2/guessing_game/target/debug/deps/guessing_game-c10f4be7107468c3: src/main.rs
-
-/Users/rodrigo/Dropbox/projects/rust/rust-playground/chapter2/guessing_game/target/debug/deps/guessing_game-c10f4be7107468c3.d: src/main.rs
-
-src/main.rs:
--- chapter2/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/rodrigo/Dropbox/projects/rust/rust-playground/chapter2/guessing_game/target/debug/guessing_game.dSYM: /Users/rodrigo/Dropbox/projects/rust/rust-playground/chapter2/guessing_game/src/main.rs
